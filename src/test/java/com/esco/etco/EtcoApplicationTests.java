25f1f067e969baec4da31fdf56ac0c7cfc4a5293
Tran Anh Toan <trananhtoan2506@gmail.com>
2026-02-03 13:43:15 +0700

    first Hello World

---
+++ src/test/java/com/esco/etco/EtcoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esco.etco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
